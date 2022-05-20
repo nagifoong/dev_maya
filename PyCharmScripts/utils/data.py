447c6df1880aa76ec18fc2cc76fa7ae463dacc07
Nagi <nagi.foong@gmail.com>
2022-05-20 21:14:01 +0700

    add convert unicode key to string

---
+++ PyCharmScripts/utils/data.py
@@ -0,0 +1,17 @@
+import collections
+
+
+def convert(data):
+    """
+    convert unicode key in dictionary to string
+    :param data:
+    :return:
+    """
+    if isinstance(data, basestring):
+        return str(data)
+    elif isinstance(data, collections.Mapping):
+        return dict(map(convert, data.iteritems()))
+    elif isinstance(data, collections.Iterable):
+        return type(data)(map(convert, data))
+    else:
+        return data
